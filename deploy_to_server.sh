1fa2b3ab4edb88416b1cb6d0a059dcd5e9cdb227
muratjumashev <jumasheff@gmail.com>
2020-02-29 15:18:25 +0600

    Add deployment script

---
+++ deploy_to_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tar -cf codomodo.tar config/ courses/ server_configs/ static/ users/ manage.py requirements.txt
+scp codomodo.tar almaz@104.248.142.48:lessons/
+rm  codomodo.tar
+ssh -tt almaz@104.248.142.48 << END
+    cd lessons/
+    tar -xf codomodo.tar
+
+    source env/bin/activate
+    pip install -r requirements.txt
+    pip install --upgrade pip
+    python manage.py migrate --noinput --settings=config.settings_prod
+    python manage.py collectstatic --noinput
+    exit
+END
