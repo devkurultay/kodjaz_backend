c217ff9adb5ab3e467642bbc1fef9d8b97ebebbf
muratjumashev <jumasheff@gmail.com>
2020-02-29 12:14:21 +0600

    Remove dup

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'static'),
