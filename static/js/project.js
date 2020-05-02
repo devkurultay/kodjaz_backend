fcf0e479330077a43a4bbf192412fca44a20c908
muratjumashev <jumasheff@gmail.com>
2020-05-02 17:15:28 +0600

    Move js to its own file

--- static/js/project.js
+++ static/js/project.js
@@ -1,1 +1,174 @@
-/* Project specific Javascript goes here. */
+(function(someObject){
+
+  var APPEND_MODE = 'a'
+
+  $(document).ready(writeEditorContentToFileTextArea)
+  $('#run').on('click', runit)
+
+  var editor = ace.edit("code")
+  editor.setTheme("ace/theme/vibrant_ink")
+  editor.session.setMode("ace/mode/python")
+
+  // Textarea for working with a "file"
+  var fileTextAreaId = "file.txt"
+  var fileTextArea = document.getElementById(fileTextAreaId)
+
+  var fileEditor = ace.edit("file")
+  fileEditor.setTheme("ace/theme/vibrant_ink")
+  fileEditor.session.setMode("ace/mode/text")
+
+  function writeEditorContentToFileTextArea() {
+    var val = fileEditor.getValue()
+    fileTextArea.innerHTML = val
+  }
+
+  fileEditor.on('change', function() {
+    writeEditorContentToFileTextArea()
+  })
+
+  // Write text editor value to the textarea
+  $('#file-tab').on('shown.bs.tab', function(e) {
+    fileEditor.focus()
+    fileEditor.resize()
+    fileEditor.execCommand("gotolineend")
+  })
+
+  function outf(text) {
+    var mypre = document.getElementById(someObject.outputElementId)
+    text = text.replace(/</g, '&lt;')
+    mypre.innerHTML = mypre.innerHTML + text
+  }
+
+  function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+      throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+  }
+
+  function filewrite(fileObj, x) {
+    var fileTextAreaNode = document.getElementById(fileObj.name)
+    var valueToWrite = x.v
+    var mode = fileObj.mode.v
+    var isAppendMode = mode === APPEND_MODE
+    if (isAppendMode) {
+      var currentText = fileTextAreaNode.innerHTML
+      valueToWrite = currentText + valueToWrite
+    }
+    // Update fileTextArea and the editor content
+    fileTextAreaNode.innerHTML = valueToWrite
+    fileEditor.session.setValue(valueToWrite)
+  }
+
+  function runit() {
+    var prog = editor.getValue();
+    var unitTest = document.getElementById('code_checker').value;
+    var readyProg = prog + '\n' + unitTest;
+    var outputElement = document.getElementById(someObject.outputElementId);
+    outputElement.innerHTML = '';
+    Sk.pre = someObject.outputElementId;
+    Sk.configure({
+      output: outf,
+      inBrowser: true,
+      read: builtinRead,
+      nonreadopen: true,
+      filewrite: filewrite,
+      killableWhile: true,
+      killableFor: true,
+      __future__: Sk.python3,
+    });
+    (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle-canvas';
+    var evalPromise = Sk.misceval.asyncToPromise(function () {
+      return Sk.importMainWithBody("<stdin>", false, readyProg, true);
+    });
+    evalPromise.then(function (mod) {
+      if (checkInput(prog) && checkOutput()) {
+        createSubmissionAndShowModal(prog, true);
+      } else {
+        createSubmissionAndShowModal(prog, false);
+      }
+    },
+    function (err) {
+      createSubmissionAndShowModal(prog, false, err.toString());
+    });
+  }
+
+  function toggleHint() {
+    $('#hint').toggleClass('hidden')
+  }
+
+  function checkInput(codeText) {
+    var positiveAssertionWords = someObject.inputShouldContain
+    var negativeAssertionWords = someObject.inputShouldNotContain
+    return _checkImpl(positiveAssertionWords, codeText, true) && _checkImpl(negativeAssertionWords, codeText, false)
+  }
+
+  function checkOutput() {
+    var output = document.getElementById('output').innerHTML.trim();
+    var positiveAssertionWords = outputShouldContain
+    var negativeAssertionWords = outputShouldNotContain
+    negativeAssertionWords.push('exception');
+    negativeAssertionWords.push('Fail:');
+    return _checkImpl(positiveAssertionWords, output, true) && _checkImpl(negativeAssertionWords, output, false)
+  }
+
+  function _checkImpl(assertionWords, textToCheck, positive) {
+    var resultsInd = [];
+    if (assertionWords.length >= 1 && assertionWords[0].length > 0) {
+      for (var i = 0; i < assertionWords.length; i++) {
+          resultsInd.push(textToCheck.indexOf(assertionWords[i]))
+      }
+    } else {
+      return true
+    }
+    return positive ? resultsInd.indexOf(-1) === -1 : resultsInd.indexOf(-1) > -1;
+  }
+
+  function createSubmissionAndShowModal(prog, passed, err) {
+    err === undefined ? err = '' : err;
+    $.ajax({
+      url: someObject.path,
+      type: 'POST',
+      data: {
+        csrfmiddlewaretoken: someObject.csrfToken,
+        exercise: someObject.objectId,
+        submitted_code: prog,
+        text_file_content: fileEditor.getValue(),
+        passed: passed
+      }
+    })
+    .done(function (responseData) {
+      if (responseData.saved) {
+        passed ? _toggleSuccessModal(true) : _toggleFailModal(err, true)
+      } else {
+        responseData.not_logged_in
+          ? passed ? _toggleSuccessModal(false) : _toggleFailModal(err, false)
+          : $("#serverErrorModal").modal('toggle')
+      }
+    })
+  }
+
+  var notLoggedInMessageContainer = '<p id="notLoggedInMessage">' + someObject.notLoggedInMessage + '</p>';
+  var notLoggedInMessageElement = $("#notLoggedInMessage");
+
+  function _toggleSuccessModal(loggedIn) {
+    if(!loggedIn) {
+      notLoggedInMessageElement.remove();
+      $(".bd-success-modal-row").append(notLoggedInMessageContainer);
+    }
+    $("#successModal").modal('toggle')
+  }
+
+  function _toggleFailModal(err, loggedIn) {
+    var failModalRow = $(".bd-fail-modal-row");
+    if(err.length > 0) {
+      $("#parseError").remove();
+      failModalRow.append('<p id="parseError">' + err + '</p>');
+    }
+    if(!loggedIn) {
+      notLoggedInMessageElement.remove();
+      failModalRow.append(notLoggedInMessageContainer)
+    }
+    $("#failModal").modal('toggle');
+  }
+
+})(someObject);
