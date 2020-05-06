7e724a9529944ab242c1b7956951dc2d0336b62d
muratjumashev <jumasheff@gmail.com>
2020-05-06 17:50:12 +0600

    Add authentication app

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from rest_framework_simplejwt import views as jwt_views
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
