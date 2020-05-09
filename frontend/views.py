59e8187566477933c9622f77325994c6ad03a3de
muratjumashev <jumasheff@gmail.com>
2020-05-09 10:08:43 +0600

    Conditionally run web socket client (only if DEBUG=True)

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    context = { 'test': 'test' }
+    context = { 'is_dev': settings.DEBUG }
     return render(request, 'frontend/index.html', context)
