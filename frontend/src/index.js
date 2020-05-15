ed671ae6415694f7dd60f50b304357e94b3179c3
muratjumashev <jumasheff@gmail.com>
2020-05-15 18:31:10 +0600

    Render tree on the main page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,7 @@
-const arr = [1, 2, 3];
-const result = () => console.log(...arr);
-// check console - Destructuring assignment is a cool feature that came along with ES6
-result();
-
 import bsStyles from 'bootstrap/dist/css/bootstrap.min.css'
 import appStyles from './styles/styles.scss'
 import '@trendmicro/react-sidenav/dist/react-sidenav.css'
+import 'react-sortable-tree/style.css'
 import '@fortawesome/fontawesome-free/js/all'
 
 import 'bootstrap'
