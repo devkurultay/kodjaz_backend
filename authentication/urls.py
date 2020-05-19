a4959ff64850074407c3c2b24b8a841d4b862f54
muratjumashev <jumasheff@gmail.com>
2020-05-19 11:01:57 +0600

    Remove token verify url

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,5 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 ]
