3ef305f5f3f23f8f66c300b99fddfd6738c092ed
muratjumashev <jumasheff@gmail.com>
2020-05-22 10:34:20 +0600

    Add redux-promise-middleware package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
 import { BrowserRouter } from "react-router-dom"
 
 import cabinet from './reducers/reducers'
 
 const store = createStore(
   cabinet,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, promise)
 )
 
 import Routes from './Routes'
