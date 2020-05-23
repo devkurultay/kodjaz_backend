df906658f7dc3dac10a1fdc86761b8bebfb17130
jumasheff <jumasheff@gmail.com>
2020-05-23 20:54:26 +0600

    Fix uncontrolled input of type ...(#16)

--- frontend/src/components/form/ExerciseForm.js
+++ frontend/src/components/form/ExerciseForm.js
@@ -120,7 +120,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Exercise name"
             onChange={(e) => handleFieldChange('name', e.target.value)}
-            value={exerciseData?.name} />
+            value={exerciseData?.name || ''} />
         </Form.Group>
         <Form.Group controlId="lecture">
           <Form.Label>Lecture text</Form.Label>
@@ -128,7 +128,7 @@ const ExerciseForm = ({
             as="textarea"
             rows="3"
             onChange={(e) => handleFieldChange('lecture', e.target.value)}
-            value={exerciseData?.lecture} />
+            value={exerciseData?.lecture || ''} />
         </Form.Group>
         <Form.Group controlId="instruction">
           <Form.Label>Instruction text</Form.Label>
@@ -136,7 +136,7 @@ const ExerciseForm = ({
             as="textarea"
             rows="3"
             onChange={(e) => handleFieldChange('instruction', e.target.value)}
-            value={exerciseData?.instruction} />
+            value={exerciseData?.instruction || ''} />
         </Form.Group>
         <Form.Group controlId="hint">
           <Form.Label>Hint text</Form.Label>
@@ -144,7 +144,7 @@ const ExerciseForm = ({
             as="textarea"
             rows="3"
             onChange={(e) => handleFieldChange('hint', e.target.value)}
-            value={exerciseData?.hint} />
+            value={exerciseData?.hint || ''} />
         </Form.Group>
         <label className="form-label" htmlFor="defaultCode">Default code</label>
         <AceEditor
@@ -153,7 +153,7 @@ const ExerciseForm = ({
           onChange={(val) => handleFieldChange('default_code', val)}
           name="defaultCode"
           height="250px"
-          value={exerciseData?.default_code}
+          value={exerciseData?.default_code || ''}
           editorProps={{ $blockScrolling: true }}
         />
         <Form.Group controlId="keywordsShouldBePresented">
@@ -162,7 +162,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: def,print,Hello world!"
             onChange={(e) => handleFieldChange('input_should_contain', e.target.value)}
-            value={exerciseData?.input_should_contain}
+            value={exerciseData?.input_should_contain || ''}
           />
         </Form.Group>
         <Form.Group controlId="keywordsShouldNotBePresented">
@@ -171,7 +171,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: class,return,Hi world!"
             onChange={(e) => handleFieldChange('input_should_not_contain', e.target.value)}
-            value={exerciseData?.input_should_not_contain}
+            value={exerciseData?.input_should_not_contain || ''}
             />
         </Form.Group>
         <Form.Group controlId="errorTextExpectedInputNotFound">
@@ -180,7 +180,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: Oops, did you forget to use print() statement?"
             onChange={(e) => handleFieldChange('input_error_text', e.target.value)}
-            value={exerciseData?.input_error_text}
+            value={exerciseData?.input_error_text || ''}
           />
         </Form.Group>
         <Form.Group controlId="keywordsShouldBeInOutput">
@@ -189,7 +189,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: Hello world!"
             onChange={(e) => handleFieldChange('output_should_contain', e.target.value)}
-            value={exerciseData?.output_should_contain}
+            value={exerciseData?.output_should_contain || ''}
           />
         </Form.Group>
         <Form.Group controlId="keywordsShouldNotBeInOutput">
@@ -198,7 +198,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: Bye, world!"
             onChange={(e) => handleFieldChange('output_should_not_contain', e.target.value)}
-            value={exerciseData?.output_should_not_contain}
+            value={exerciseData?.output_should_not_contain || ''}
           />
         </Form.Group>
         <Form.Group controlId="errorTextExpectedOutputNotShown">
@@ -207,7 +207,7 @@ const ExerciseForm = ({
             type="text"
             placeholder="Example: Oops, did you forget to print() Hello world!?"
             onChange={(e) => handleFieldChange('output_error_text', e.target.value)}
-            value={exerciseData?.output_error_text}
+            value={exerciseData?.output_error_text || ''}
           />
         </Form.Group>
         <label className="form-label" htmlFor="unitTestCode">Unit test code</label>
@@ -217,25 +217,32 @@ const ExerciseForm = ({
           name="unitTestCode"
           height="250px"
           onChange={(val) => handleFieldChange('unit_test', val)}
-          value={exerciseData?.unit_test}
+          value={exerciseData?.unit_test || ''}
           editorProps={{ $blockScrolling: true }}
         />
         <Form.Group controlId="prevExercise">
           <Form.Label>Select previous exercise</Form.Label>
-          <Form.Control type="text" onFocus={(e) => handleEntityPick(e, 'Exercise')}/>
+          <Form.Control
+            type="text"
+            readOnly
+            onFocus={(e) => handleEntityPick(e, 'Exercise')}/>
         </Form.Group>
         <Form.Group controlId="isPublishedCheckbox">
           <Form.Check
             type="checkbox"
             id="is_published"
-            checked={exerciseData?.is_published}
+            checked={!!exerciseData?.is_published}
             onChange={(e) => handleFieldChange('is_published', e.target.checked)}
             label="Is the exercise published?"
           />
         </Form.Group>
         <Form.Group controlId="belongsToLesson">
           <Form.Label>Lesson the exercise belongs to</Form.Label>
-          <Form.Control type="text" value={lesson?.name} onFocus={(e) => handleEntityPick(e, 'Lesson')} />
+          <Form.Control
+            type="text"
+            readOnly
+            onFocus={(e) => handleEntityPick(e, 'Lesson')}
+            value={lesson?.name || ''} />
         </Form.Group>
         <Form.Group controlId="fileTxt">
           <Form.Label>If this field has a content, file.txt tab will be shown</Form.Label>
@@ -243,7 +250,7 @@ const ExerciseForm = ({
             as="textarea"
             rows="3"
             onChange={(e) => handleFieldChange('text_file_content', e.target.value)}
-            value={exerciseData?.text_file_content} />
+            value={exerciseData?.text_file_content || ''} />
         </Form.Group>
       </Form>
       <Button variant="primary" onClick={handleSave}>
