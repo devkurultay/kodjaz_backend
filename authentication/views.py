e0a75039cb7716c8507bff0c0a13d325c74b6ccf
muratjumashev <jumasheff@gmail.com>
2020-06-06 14:27:50 +0600

    Add logout endpoint

---
+++ authentication/views.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions, status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from .serializers import RefreshTokenSerializer
+ 
+
+class LogoutView(GenericAPIView):
+    serializer_class = RefreshTokenSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, *args):
+        sz = self.get_serializer(data=request.data)
+        sz.is_valid(raise_exception=True)
+        sz.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
