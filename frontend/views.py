60d2dc7e60c4038ae0f2dca3cdc7e4a23856cdbd
muratjumashev <jumasheff@gmail.com>
2020-06-07 14:01:35 +0600

    Only admins are allowed to enter cabinet

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,14 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
+
+def is_staff(user):
+    return user.is_staff
+
+
+@login_required
+@user_passes_test(is_staff)
 def index(request):
     context = { 'is_dev': settings.DEBUG }
     return render(request, 'frontend/index.html', context)
