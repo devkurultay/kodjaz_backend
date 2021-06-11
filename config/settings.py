2f0ea256e5934ab087e958f76382d0a01a135f1f
muratjumashev <jumasheff@gmail.com>
2021-06-11 20:22:51 +0600

    Fix auth issues

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
     # Spirit
-    'spirit.user.auth.backends.UsernameAuthBackend',
     'spirit.user.auth.backends.EmailAuthBackend',
 )
 
