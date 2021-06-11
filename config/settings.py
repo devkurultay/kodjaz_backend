f36feabf0d7ae9c55321346ac7c2e559e6e978f1
muratjumashev <jumasheff@gmail.com>
2021-06-11 19:26:46 +0600

    Comment failing hasher

--- config/settings.py
+++ config/settings.py
@@ -229,7 +229,7 @@ AUTH_PASSWORD_VALIDATORS = [
 PASSWORD_HASHERS = [
     'authentication.hashers.CustomBCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    #'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
 ]
