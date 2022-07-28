cea1d6d10d61ff294386251cc74df8124ee08821
murat <jumasheff@gmail.com>
2022-07-28 12:57:09 +0600

    Read config data from file

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-npm run build
+# npm run build
 source env/bin/activate
 python manage.py collectstatic --noinput
+# Bundle up an archive file
 tar -cf codomodo.tar authentication/ config/ courses/ frontend/ server_configs/ staticfiles/ requirements/ users/ manage.py robots.txt
-scp codomodo.tar almaz@104.248.142.48:lessons/
-rm  codomodo.tar
-ssh -tt almaz@104.248.142.48 << END
-    cd lessons/
+# Load variables form .env file
+source .env
+# Upload bundled archive
+scp codomodo.tar $SERVER_USERNAME@$SERVER_IP:$PROJECT_FOLDER_ON_SERVER/
+rm codomodo.tar
+ssh -tt $SERVER_USERNAME@$SERVER_IP << END
+    cd $PROJECT_FOLDER_ON_SERVER/
     tar -xf codomodo.tar
-
-    source env/bin/activate
-    pip install -r requirements/requirements_prod.txt
+    source env/bin/activate && echo 'Activated env' || sudo apt install python3.8-venv --yes && python3 -m venv env && source env/bin/activate && echo 'Installed venv and activated env';
+    echo "Now installing python packages"
     pip install --upgrade pip
+    pip install -r requirements/requirements_prod.txt
+    echo "Applying migrations"
     python manage.py migrate --noinput --settings=config.settings_prod
+    echo "Exiting"
     exit
 END
