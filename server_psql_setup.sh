fd2bd0998326d29507cb6c7e08b2463377a8c92f
murat <jumasheff@gmail.com>
2022-07-28 13:26:24 +0600

    Add a script for setting up postgresql on server

---
+++ server_psql_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source .env
+ssh -tt $SERVER_USERNAME@$SERVER_IP << END
+  sudo apt install python3-dev libpq-dev postgresql postgresql-contrib --yes
+  sudo -u postgres psql
+  CREATE DATABASE $DB_NAME;
+  CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+  ALTER ROLE $DB_USER SET client_encoding TO 'utf8';
+  ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';
+  ALTER ROLE $DB_USER SET timezone TO 'Asia/Bishkek';
+  GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+  \q
+  exit;
+END
