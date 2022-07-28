fc957acd5f7dc0ed54659eaf7a982aaa9375c632
murat <jumasheff@gmail.com>
2022-07-28 13:27:03 +0600

    Copy over local .env to server and set debug=false

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -3,7 +3,7 @@
 source env/bin/activate
 python manage.py collectstatic --noinput
 # Bundle up an archive file
-tar -cf codomodo.tar authentication/ config/ courses/ frontend/ server_configs/ staticfiles/ requirements/ users/ manage.py robots.txt
+tar -cf codomodo.tar authentication/ config/ courses/ frontend/ server_configs/ staticfiles/ requirements/ users/ manage.py robots.txt .env
 # Load variables form .env file
 source .env
 # Upload bundled archive
@@ -12,6 +12,9 @@ rm codomodo.tar
 ssh -tt $SERVER_USERNAME@$SERVER_IP << END
     cd $PROJECT_FOLDER_ON_SERVER/
     tar -xf codomodo.tar
+
+    sed -i 's/DEBUG=True/DEBUG=False/' .env
+
     source env/bin/activate && echo 'Activated env' || sudo apt install python3.8-venv --yes && python3 -m venv env && source env/bin/activate && echo 'Installed venv and activated env';
     echo "Now installing python packages"
     pip install --upgrade pip
