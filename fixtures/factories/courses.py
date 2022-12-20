274299802ba199605006b28095c294b6d63c70d0
murat <jumasheff@gmail.com>
2022-12-20 12:53:20 +0600

    Add UnitFactory

--- fixtures/factories/courses.py
+++ fixtures/factories/courses.py
@@ -1,6 +1,7 @@
 import factory
 
 from courses.models import Track
+from courses.models import Unit
 
 
 class TrackFactory(factory.django.DjangoModelFactory):
@@ -11,3 +12,13 @@ class TrackFactory(factory.django.DjangoModelFactory):
     description = factory.Faker('name')
     is_published = False
     programming_language = factory.Faker('name')
+
+
+class UnitFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Unit
+    
+    name = factory.Faker('name')
+    description = factory.Faker('name')
+    is_published = False
+    track = factory.SubFactory('fixtures.factories.courses.TrackFactory')
