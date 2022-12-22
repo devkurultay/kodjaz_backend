f786c4a088277a80180354624224e6745ba29084
murat <jumasheff@gmail.com>
2022-12-22 17:29:26 +0600

    Adapt view to serve react app

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,13 @@
+import json
 from django.conf import settings
 from django.template import loader
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 from rest_framework_simplejwt.tokens import RefreshToken
+from .utils import static_fallback_open
+
 
 def is_staff(user):
     return user.is_staff
@@ -12,7 +16,33 @@ def is_staff(user):
 @login_required
 @user_passes_test(is_staff)
 def index(request):
-    context = { 'is_dev': settings.DEBUG }
+    """
+    Example contents of asset-manifest.json
+    {
+        "files": {
+            "main.css": "/static/css/main.d6678dc7.css",
+            "main.js": "/static/js/main.d996b665.js",
+            "index.html": "/index.html",
+            "main.d6678dc7.css.map": "/static/css/main.d6678dc7.css.map",
+            "main.d996b665.js.map": "/static/js/main.d996b665.js.map"
+        },
+        "entrypoints": [
+            "static/css/main.d6678dc7.css",
+            "static/js/main.d996b665.js"
+        ]
+    }
+    """
+    asset_manifest = {}
+    with static_fallback_open("asset-manifest.json") as json_file:
+        asset_manifest = json.load(json_file)
+    react_css_bundle = asset_manifest["files"]["main.css"]
+    react_js_bundle = asset_manifest["files"]["main.js"]
+
+    context = {
+        'is_dev': settings.DEBUG,
+        "react_css_bundle": react_css_bundle,
+        "react_js_bundle": react_js_bundle
+    }
     content = loader.render_to_string('frontend/index.html', context, request)
     response = HttpResponse(content)
     # Set tokens (if the code executes here, then it's a is_staff user)
