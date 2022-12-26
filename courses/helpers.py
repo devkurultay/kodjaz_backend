b4e95a0ba4d9b6837f2b5cd10f1d9f161c33c3bd
murat <jumasheff@gmail.com>
2022-12-26 12:10:36 +0600

    Add Checker class

--- courses/helpers.py
+++ courses/helpers.py
@@ -34,4 +34,111 @@ def run_code(submitted_code, programming_language):
         payload = {'answer': submitted_code}
         res = requests.post(url, json=payload, headers=headers)
         return res.json()
-    return 'output'
+    raise NotImplementedError(f'{programming_language} is not supported')
+
+
+class InputObject:
+    def __init__(
+        self,
+        programming_language,
+        code,
+        unit_test,
+        input_should_contain,
+        input_should_contain_error_msg,
+        input_should_not_contain,
+        input_should_not_contain_error_msg,
+        output_should_contain,
+        output_should_contain_error_msg,
+        output_should_not_contain,
+        output_should_not_contain_error_msg
+    ) -> None:
+        self.programming_language = programming_language
+        self.code = code
+        self.unit_test = unit_test
+        self.input_should_contain = input_should_contain
+        self.input_should_contain_error_msg = input_should_contain_error_msg
+        self.input_should_not_contain = input_should_not_contain
+        self.input_should_not_contain_error_msg = input_should_not_contain_error_msg
+        self.output_should_contain = output_should_contain
+        self.output_should_contain_error_msg = output_should_contain_error_msg
+        self.output_should_not_contain = output_should_not_contain
+        self.output_should_not_contain_error_msg = output_should_not_contain_error_msg
+
+
+class Checker:
+    def __init__(self, inp_obj) -> None:
+        self.programming_language = inp_obj.programming_language
+        self.code = inp_obj.code
+        self.unit_test = inp_obj.unit_test
+
+        self.inp_should_contain = inp_obj.input_should_contain
+        self.inp_should_contain_error_msg = inp_obj.input_should_contain_error_msg
+
+        self.inp_should_not_contain = inp_obj.input_should_not_contain
+        self.inp_should_not_contain_error_msg = inp_obj.input_should_not_contain_error_msg
+
+        self.out_should_contain = inp_obj.output_should_contain
+        self.out_should_contain_error_msg = inp_obj.output_should_contain_error_msg
+
+        self.out_should_not_contain = inp_obj.output_should_not_contain
+        self.out_should_not_contain_error_msg = inp_obj.output_should_not_contain_error_msg
+
+    def _check_source_contains(self, source, should_contain, should_contain_error_msg):
+        if should_contain:
+            wanted_items = should_contain.split(',')
+            for item in wanted_items:
+                if item not in source:
+                    return False, should_contain_error_msg.format(item)
+        return True, ''
+    
+    def _check_source_does_not_contain(self, source, should_not_contain, should_not_contain_error_msg):
+        if should_not_contain:
+            unwanted_items = should_not_contain.split(',')
+            for item in unwanted_items:
+                if item in source:
+                    return False, should_not_contain_error_msg.format(item)
+        return True, ''
+
+    def _check_input_contains(self):
+        return self._check_source_contains(
+            self.code, self.inp_should_contain, self.inp_should_contain_error_msg)
+
+    def _check_input_does_not_contain(self):
+        return self._check_source_does_not_contain(
+            self.code, self.inp_should_not_contain, self.inp_should_not_contain_error_msg)
+
+    def _run_code(self):
+        code_to_run = self.code + '\n' + (self.unit_test if self.unit_test else '')
+        return run_code(code_to_run, self.programming_language)
+
+    def _output_contains(self, output):
+        return self._check_source_contains(
+            output, self.out_should_contain, self.out_should_contain_error_msg)
+
+    def _output_does_not_contain(self, output):
+        return self._check_source_does_not_contain(
+            output, self.out_should_not_contain, self.out_should_not_contain_error_msg)
+    
+    def _build_output(self, success, console_output, error_msg):
+        return {
+            'success': success,
+            'console_output': console_output,
+            'error_msg': error_msg
+        }
+
+    def check(self):
+        is_input_ok, error_msg = self._check_input_contains()
+        if not is_input_ok:
+            return self._build_output(False, '', error_msg)
+        is_input_ok, error_msg = self._check_input_does_not_contain()
+        if not is_input_ok:
+            return self._build_output(False, '', error_msg)
+
+        output = self._run_code()
+        is_output_ok, error_msg = self._output_contains(output)
+        if not is_output_ok:
+            return self._build_output(False, output, error_msg)
+        is_output_ok, error_msg = self._output_does_not_contain(output)
+        if not is_output_ok:
+            return self._build_output(False, output, error_msg)
+        return self._build_output(True, output, error_msg)
--- courses/tests/helpers_tests.py
+++ courses/tests/helpers_tests.py
@@ -6,6 +6,8 @@ from django.conf import settings
 
 from courses.helpers import run_code
 from courses.helpers import check_text_source
+from courses.helpers import InputObject
+from courses.helpers import Checker
 
 
 class HelpersTests(APITestCase):
@@ -92,3 +94,152 @@ class CheckTextSourceTest(APITestCase):
             output_text, input_should_contain, input_should_not_contain, '')
         self.assertTrue(res[0])
         self.assertEqual(res[1], '')
+
+
+class CheckerTests(APITestCase):
+
+    def test_input_should_not_contain_error_msg_is_returned_when_input_has_no_required_item(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    print('hi')",
+            unit_test="",
+            input_should_contain='sum,return',
+            input_should_contain_error_msg='Your code should contain {} keyword',
+            input_should_not_contain='print',
+            input_should_not_contain_error_msg='Your code should not contain print() function',
+            output_should_contain='',
+            output_should_contain_error_msg='',
+            output_should_not_contain='',
+            output_should_not_contain_error_msg=''
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertFalse(result['success'])
+        self.assertEqual(result['console_output'], '')
+        self.assertEqual(
+            result['error_msg'],
+            input_object.input_should_contain_error_msg.format('return'))
+
+    def test_input_should_not_contain_error_msg_is_returned_when_input_has_illegal_items(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    print('hi')\nreturn x+y",
+            unit_test="assert sum(3,3) == 6",
+            input_should_contain='return',
+            input_should_contain_error_msg='Your code should contain return statement',
+            input_should_not_contain='print',
+            input_should_not_contain_error_msg='Your code should not contain {} keyword',
+            output_should_contain='',
+            output_should_contain_error_msg='',
+            output_should_not_contain='',
+            output_should_not_contain_error_msg=''
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertFalse(result['success'])
+        self.assertEqual(result['console_output'], '')
+        self.assertEqual(
+            result['error_msg'],
+            input_object.input_should_not_contain_error_msg.format('print'))
+    
+    def test_output_should_contain_error_msg_is_returned_when_output_has_no_required_item(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    return x+y",
+            unit_test="sum(3,3)",
+            input_should_contain='',
+            input_should_contain_error_msg='',
+            input_should_not_contain='',
+            input_should_not_contain_error_msg='',
+            output_should_contain='6',
+            output_should_contain_error_msg='Your output should contain {}. But it doesn\'t.',
+            output_should_not_contain='',
+            output_should_not_contain_error_msg=''
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertFalse(result['success'])
+        self.assertEqual(result['console_output'], '')
+        self.assertEqual(
+            result['error_msg'],
+            input_object.output_should_contain_error_msg.format('6'))
+    
+    def test_output_should_not_contain_error_msg_is_returned_when_output_has_illegal_items(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    print('hi')",
+            unit_test="sum(3,3)",
+            input_should_contain='',
+            input_should_contain_error_msg='',
+            input_should_not_contain='',
+            input_should_not_contain_error_msg='',
+            output_should_contain='',
+            output_should_contain_error_msg='',
+            output_should_not_contain='hi',
+            output_should_not_contain_error_msg='You should have removed "{}". But it looks like you haven\'t'
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertFalse(result['success'])
+        self.assertEqual(
+            result['error_msg'],
+            input_object.output_should_not_contain_error_msg.format('hi'))
+
+    def test_happy_case_empty_match_checks(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    print(3+3)",
+            unit_test="sum(3,3)",
+            input_should_contain='',
+            input_should_contain_error_msg='',
+            input_should_not_contain='',
+            input_should_not_contain_error_msg='',
+            output_should_contain='',
+            output_should_contain_error_msg='',
+            output_should_not_contain='hi',
+            output_should_not_contain_error_msg=''
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertTrue(result['success'])
+        self.assertEqual(result['console_output'], '6\n')
+        self.assertEqual(result['error_msg'], '')
+    
+    def test_happy_case_all_checks_pass(self):
+        input_object = InputObject(
+            programming_language='Python',
+            code="def sum(x,y):\n    print(3+3)",
+            unit_test="sum(3,3)",
+            input_should_contain='print',
+            input_should_contain_error_msg='Your code should contain {}. But it does not',
+            input_should_not_contain='return',
+            input_should_not_contain_error_msg='Your code should not contain {}. But it does',
+            output_should_contain='6\n',
+            output_should_contain_error_msg='The output should contain {}. But it does not',
+            output_should_not_contain='hi',
+            output_should_not_contain_error_msg='The output should not contain {}. But it contains'
+        )
+        checker = Checker(input_object)
+        result = checker.check()
+        self.assertTrue(result['success'])
+        self.assertEqual(result['console_output'], '6\n')
+        self.assertEqual(result['error_msg'], '')
+    
+    def test_(self):
+        not_supported_language = 'Python124'
+        input_object = InputObject(
+            programming_language=not_supported_language,
+            code="def sum(x,y):\n    print(3+3)",
+            unit_test="sum(3,3)",
+            input_should_contain='print',
+            input_should_contain_error_msg='Your code should contain {}. But it does not',
+            input_should_not_contain='return',
+            input_should_not_contain_error_msg='Your code should not contain {}. But it does',
+            output_should_contain='6\n',
+            output_should_contain_error_msg='The output should contain {}. But it does not',
+            output_should_not_contain='hi',
+            output_should_not_contain_error_msg='The output should not contain {}. But it contains'
+        )
+        checker = Checker(input_object)
+        with self.assertRaisesMessage(NotImplementedError, f'{not_supported_language} is not supported'):
+            checker.check()
