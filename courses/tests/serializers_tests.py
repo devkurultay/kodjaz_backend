8b0a2fc5d1279983349d7bd1adc115e4ede80e7a
murat <jumasheff@gmail.com>
2023-01-09 18:07:26 +0600

    Add UserUnitSerializerTests

--- courses/tests/serializers_tests.py
+++ courses/tests/serializers_tests.py
@@ -2,10 +2,11 @@ from django.test.testcases import TestCase
 from fixtures.factories.courses import ExerciseFactory
 from fixtures.factories.courses import LessonFactory
 from fixtures.factories.courses import SubmissionFactory
-from fixtures.factories.user import UserFactory
+from fixtures.factories.courses import UnitFactory
 
 from courses.serializers import UserExerciseSerializer
 from courses.serializers import UserLessonSerializer
+from courses.serializers import UserUnitSerializer
 
 
 class UserExerciseSerializerTests(TestCase):
@@ -53,5 +54,33 @@ class UserLessonSerializerTests(TestCase):
         kwargs = {'context': {'user': self.failed_submission.user}}
         serializer = UserLessonSerializer(
             instance=self.incomplete_lesson, **kwargs)
+        data = serializer.data
+        self.assertFalse(data['is_complete'])
+
+
+class UserUnitSerializerTests(TestCase):
+
+    def setUp(self) -> None:
+        self.complete_unit = UnitFactory()
+        complete_lesson = LessonFactory(unit=self.complete_unit)
+        exercise = ExerciseFactory(lesson=complete_lesson)
+        self.submission = SubmissionFactory(exercise=exercise, passed=True)
+
+        self.incomplete_unit = UnitFactory()
+        incomplete_lesson = LessonFactory(unit=self.incomplete_unit)
+        incomplete_exercise = ExerciseFactory(lesson=incomplete_lesson)
+        self.failed_submission = SubmissionFactory(
+            exercise=incomplete_exercise, passed=False)
+        return super().setUp()
+
+    def test_is_complete_property_field(self):
+        kwargs = {'context': {'user': self.submission.user}}
+        serializer = UserUnitSerializer(instance=self.complete_unit, **kwargs)
+        data = serializer.data
+        self.assertTrue(data['is_complete'])
+
+        kwargs = {'context': {'user': self.failed_submission.user}}
+        serializer = UserUnitSerializer(
+            instance=self.incomplete_unit, **kwargs)
         data = serializer.data
         self.assertFalse(data['is_complete'])
