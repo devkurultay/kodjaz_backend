824613e29e9b9e96ccb607dd8702450bf8a13971
murat <jumasheff@gmail.com>
2023-01-09 18:17:56 +0600

    Temp disable postgres db for dev env

--- config/settings.py
+++ config/settings.py
@@ -120,12 +120,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': ROOT_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
