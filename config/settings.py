b8cd4fc705c92d9be1abe8d80dc1585dc181b711
murat <jumasheff@gmail.com>
2023-01-09 18:38:02 +0600

    Add DB host to .env

--- config/settings.py
+++ config/settings.py
@@ -120,10 +120,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': ROOT_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ROOT_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": os.environ.get('LOCAL_DB_HOST', 'localhost'),
+        "PORT": 5432
     }
 }
 
