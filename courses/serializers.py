1f70c797a1f82b2e7b94238f5368bb2610b8a74e
murat <jumasheff@gmail.com>
2023-01-10 22:00:41 +0600

    Remove is_complete from LessonSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -69,13 +69,12 @@ class SubmissionSerializer(serializers.ModelSerializer):
 class LessonSerializer(serializers.ModelSerializer):
     lesson_exercises = ExerciseSerializer(many=True, read_only=True)
     entity_type = serializers.SerializerMethodField()
-    is_complete = serializers.BooleanField(default=False, read_only=True)
 
     class Meta:
         model = Lesson
         fields = (
             'id', 'name', 'entity_type', 'is_published',
-            'lesson_exercises', 'unit', 'is_complete'
+            'lesson_exercises', 'unit'
         )
     
     def get_entity_type(self, obj):
