7477d6d0384768a1ecc53feca338ebbedf9d4531
murat <jumasheff@gmail.com>
2023-01-20 14:22:12 +0600

    Remove unused items

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -35,31 +35,3 @@ class RefreshTokenSerializer(serializers.Serializer):
             RefreshToken(self.token).blacklist()
         except TokenError:
             self.fail('bad_token')
-
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-  )
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('email', 'password', 'password2')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-        email=validated_data['email'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
